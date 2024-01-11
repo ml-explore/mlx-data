aca971b0b9a7a3aa099d5ac7d3231d75ebc38b0b
Angelos Katharopoulos <a_katharopoulos@apple.com>
2024-01-10 20:17:37 -0800

    Fix benchmark and occasional bus error (#32)

--- python/mlx/data/__init__.py
+++ python/mlx/data/__init__.py
@@ -1,4 +1,13 @@
 # Copyright © 2023 Apple Inc.
 
+
 from ._c import *
 from ._c import __version__
+
+# fmt: off
+# Passing strings from python to be casted by pybind11 crashes on some builds
+# if we don't import numpy.
+#
+# TODO: Evaluate if it is due to improper linking and fix correctly.
+import numpy  # isort: skip
+del numpy
