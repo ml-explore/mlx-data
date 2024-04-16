bea27c5d6ff50a30476866c90774f80bf35a0b3f
Angelos Katharopoulos <a_katharopoulos@apple.com>
2024-04-16 01:28:49 -0700

    Add a replace operation

--- python/tests/test_buffer.py
+++ python/tests/test_buffer.py
@@ -1,11 +1,11 @@
 # Copyright © 2024 Apple Inc.
 
-from unittest import TestCase
+import unittest
 
 import mlx.data as dx
 
 
-class TestBuffer(TestCase):
+class TestBuffer(unittest.TestCase):
     def test__getitem__(self):
         n = 5
         b = dx.buffer_from_vector(list(dict(i=i) for i in range(n)))
@@ -18,3 +18,7 @@ class TestBuffer(TestCase):
             _ = b[n]
         with self.assertRaises(IndexError):
             _ = b[-(n + 1)]
+
+
+if __name__ == "__main__":
+    unittest.main()
