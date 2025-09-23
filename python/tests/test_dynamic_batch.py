2abec12dc6a217e1ffa102ba355abe49f3baf092
Ronan Collobert <collobert@apple.com>
2025-09-22 18:02:30 -0700

    more tests

--- python/tests/test_dynamic_batch.py
+++ python/tests/test_dynamic_batch.py
@@ -20,6 +20,21 @@ def count_padding(sample):
 
 
 class TestDynamicBatch(unittest.TestCase):
+    def test_buffer_dynamic_batch_padding(self):
+        dset = dx.buffer_from_vector([random_sample(idx) for idx in range(10_000)])
+        # Compute the average padding size with naive batching
+        naive_padding = sum(count_padding(s) for s in dset.batch(16))
+        dynbatch_padding = sum(
+            count_padding(s)
+            for s in dset.dynamic_batch("tokens", max_data_size=16 * 1024)
+        )
+        # Count the total valid tokens
+        valid_tokens = sum(d["length"] for d in dset)
+        simple_pad_ratio = naive_padding / (valid_tokens + naive_padding)
+        dynamic_pad_ratio = dynbatch_padding / (valid_tokens + dynbatch_padding)
+        self.assertTrue(simple_pad_ratio > 0.43)
+        self.assertTrue(dynamic_pad_ratio < 0.004)
+
     def test_stream_dynamic_batch_padding(self):
         dset = dx.buffer_from_vector([random_sample(idx) for idx in range(10_000)])
         # Compute the average padding size with naive batching
@@ -37,6 +52,13 @@ class TestDynamicBatch(unittest.TestCase):
         self.assertTrue(simple_pad_ratio > 0.43)
         self.assertTrue(dynamic_pad_ratio < 0.06)
 
+    def test_buffer_dynamic_batch_indexing(self):
+        dset = dx.buffer_from_vector([random_sample(idx) for idx in range(1000)])
+        found_indices = np.zeros((1000,))
+        for s in dset.dynamic_batch("tokens", max_data_size=16 * 1024):
+            found_indices[s["idx"]] = 1
+        self.assertTrue(found_indices.sum() == 1000)
+
     def test_stream_dynamic_batch_indexing(self):
         dset = dx.buffer_from_vector([random_sample(idx) for idx in range(1000)])
         found_indices = np.zeros((1000,))
@@ -53,6 +75,18 @@ class TestDynamicBatch(unittest.TestCase):
             min_token_size = min(min_token_size, s["tokens"].size)
         self.assertTrue(max_token_size <= 16 * 1024)
         self.assertFalse(min_token_size >= 15 * 1024)
+
+    def test_stream_dynamic_batch_min_token_size(self):
+        dset = dx.buffer_from_vector([random_sample(idx) for idx in range(1000)])
+        max_token_size = 0
+        min_token_size = 1e10
+        for s in dset.to_stream().dynamic_batch(
+            512, "tokens", min_data_size=15 * 1024, max_data_size=16 * 1024
+        ):
+            max_token_size = max(max_token_size, s["tokens"].size)
+            min_token_size = min(min_token_size, s["tokens"].size)
+        self.assertTrue(max_token_size <= 16 * 1024)
+        self.assertTrue(min_token_size >= 15 * 1024)
 
 
 if __name__ == "__main__":
