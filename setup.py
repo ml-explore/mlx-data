2f431e90a06c33d2e3f78019b32c563e8fd8a71a
Angelos Katharopoulos <a_katharopoulos@apple.com>
2025-09-26 14:23:37 -0700

    Bump the version (#107)

--- setup.py
+++ setup.py
@@ -102,7 +102,7 @@ if __name__ == "__main__":
 
     setup(
         name="mlx-data",
-        version=get_version("0.1.0"),
+        version=get_version("0.2.0"),
         author="Ronan Collobert",
         author_email="collobert@apple.com",
         url="https://github.com/ml-explore/mlx-data",
